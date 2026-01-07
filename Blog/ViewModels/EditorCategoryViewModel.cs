b19be2005c1a21cde5166a8b5086cc2bcc4039ed
guiferrao <ferraogui@hotmail.com>
2026-01-06 21:47:21 -0300

    Modulo 3, CRUD de categorias e viewmodels

---
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class EditorCategoryViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatorio")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "este campo deve ter entre 3 e 40 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O slug é obrigatorio")]
+        public string Slug { get; set; }
+    }
+}
